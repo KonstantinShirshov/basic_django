90a5ef9dc4062b57311da2eef75bf00dd21c7d99
Konstantin <shirshikk@gmail.com>
2025-09-28 23:04:56 +0500

    init commit

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование продукта')
+    description = models.TextField(verbose_name='Описание', help_text='Введите описание продукта')
+    image = models.ImageField(upload_to='catalog/image', verbose_name='Изображение', help_text='Загрузите изображение продукта', blank=True, null=True)
+    price = models.IntegerField(verbose_name='Цена', help_text='Введите цену продукта')
+    created_at = models.DateField(verbose_name='Дата создания', help_text='Введите дату создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения', help_text='Введите дату последнего изменения')
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name','price']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование продукта')
+    description = models.TextField(verbose_name='Описание', help_text='Введите описание продукта')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', 'price']
--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-th7g6bydzy1dhls8tx$q!uw$vu5z6ke5u1lk)m6s2r7vc7+6go'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -49,8 +53,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
