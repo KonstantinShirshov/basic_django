c9ec1df172b6b4dfb7e12e474d6cee85255ab1ab
Konstantin <shirshikk@gmail.com>
2025-10-26 14:17:57 +0500

    finish commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductTemplateView, ContactFormView, ProductDetailView, ProductCreateView, ProductDeleteView
+from catalog.views import ProductListView, ProductTemplateView, ContactFormView, ProductDetailView, ProductCreateView, ProductDeleteView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', ContactFormView.as_view(), name='answer'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/new/', ProductCreateView.as_view(), name='product_create'),
-    path('products/<int:pk>/edit/', ProductDetailView.as_view(), name='product_update'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -31,7 +31,7 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
 
     def get_success_url(self):
-        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+        return reverse('catalog:product_update', args=[self.kwargs.get('pk')])
 
 
 class ProductDeleteView(DeleteView):
